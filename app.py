54b6ae2f7c1626ae0f5edd55a337d5cba966e042
TeamPossathorn <Possathornpkt@hotmail.com>
2024-11-03 14:39:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ st.title("Prediction Web App")
 st.write("ป้อนข้อมูลเพื่อทำนายผลลัพธ์ด้วยโมเดลที่โหลด")
 
 # สร้างอินพุตสำหรับผู้ใช้ป้อนข้อมูล
-feature_1 = st.number_input("Feature 1", min_value=0.0, max_value=100.0, value=50.0)
-feature_2 = st.number_input("Feature 2", min_value=0.0, max_value=100.0, value=50.0)
+feature_1 = st.text_area("ใส่ชื่อที่อยู่")
+#feature_2 = st.number_input("Feature 2", min_value=0.0, max_value=100.0, value=50.0)
 
 # เมื่อผู้ใช้กดปุ่ม "Predict"
 if st.button("Predict"):
     # จัดรูปแบบข้อมูลอินพุตเป็น array
-    input_data = np.array([[feature_1, feature_2]])
+    input_data = np.array([[feature_1]])
 
     # ทำการพยากรณ์
     prediction = model.predict(input_data)
