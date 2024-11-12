9478c217e6f9e5641d1a7b38bf141cc2630df577
TeamPossathorn <Possathornpkt@hotmail.com>
2024-11-12 22:27:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ if 'all_predicted_tags' not in st.session_state:
     st.session_state['all_predicted_tags'] = []
 
 # Divide layout into three columns in one row
-col1, col2 = st.columns([2, 4])
+col1, col2 = st.columns([1,1.5])
 
 # Column 1: Input Section
 # Custom CSS to adjust font size and padding for column1
@@ -143,7 +143,7 @@ st.markdown(
         padding: 4px;    /* Adjust padding for spacing within the column */
     }
     .custom-column-style h2 {
-        font-size: 20px;  /* Adjust title font size */
+        font-size: 18px;  /* Adjust title font size */
     }
     </style>
     """,
